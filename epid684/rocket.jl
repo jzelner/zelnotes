ccb8ad42fd93a37ad83bd6a670bbe37f6602372b
Jon Zelner <jzelner@gmail.com>
2023-02-15 16:14:09 -0500

    added radon tutorial

--- epid684/rocket.jl
+++ epid684/rocket.jl
@@ -2,61 +2,67 @@ using JuMP
 import Ipopt
 import Plots
 
-sir = Model(Ipopt.Optimizer)
-set_silent(sir)
+function buildmodel()
+    sir = Model(Ipopt.Optimizer)
+    set_silent(sir)
 
-p1 = 0.75
-p2 = 1 - p1
-ρ = 0.5
-w12 = (1 - ρ) * p2
-w11 = 1 - w12
+    p1 = 0.75
+    p2 = 1 - p1
+    ρ = 0.9
+    w12 = (1 - ρ) * p2
+    w11 = 1 - w12
 
-ratio = [1.0, 2.0]
-
-
-w = [w11 w12
-    1-ρ ρ]
-
-I_0 = 0.01
-S_0 = 1 - I_0
-R_0 = 0
-gamma = 0.5
-#beta = 2
-Δt = 0.1
-n = 800    # Time step
-
-@variables(sir, begin
-    # Time step 
-    # State variables
-    x[1:n, 1:2, 1:3] ≥ 0
-    beta >= 0.1
-end);
-
-for i in 1:2
-    fix(x[1, i, 1], (1 - I_0), force=true)
-    fix(x[1, i, 2], I_0, force=true)
-    fix(x[1, i, 3], 0, force=true)
-end
+    ratio = [1.0, 2.0]
 
 
-@NLconstraint(sir,(p1*x[n, 1, 3] + p2*x[n,2,3]) >= 0.1)
+    w = [w11 w12
+        1-ρ ρ]
 
-for j in 2:n
+    I_0 = 0.01
+    S_0 = 1 - I_0
+    R_0 = 0
+    gamma = 0.5
+    #beta = 2
+    Δt = 0.1
+    n = 800    # Time step
+
+    @variables(sir, begin
+        # Time step 
+        # State variables
+        x[1:n, 1:2, 1:3] ≥ 0
+        beta >= 0.1
+    end)
+
     for i in 1:2
-        @NLconstraint(sir, x[j, i, 2] == x[j-1, i, 2] + Δt * ((-x[j-1, i, 2] * gamma) + (beta * ratio[i] * x[j-1, i, 1] * (w[i, 1] * x[j-1, 1, 2] + w[i, 2] * x[j-1, 2, 2]))))
-
-        @NLconstraint(sir, x[j, i, 1] == x[j-1, i, 1] + Δt * (-beta * ratio[i] * x[j-1, i, 1] * (w[i, 1] * x[j-1, 1, 2] + w[i, 2] * x[j-1, 2, 2])))
-
-        @NLconstraint(sir, x[j, i, 3] == x[j-1, i, 3] + Δt * (x[j-1, i, 2] * gamma))
+        fix(x[1, i, 1], (1 - I_0), force=true)
+        fix(x[1, i, 2], I_0, force=true)
+        fix(x[1, i, 3], 0, force=true)
     end
+
+
+    @NLconstraint(sir, (p1 * x[n, 1, 3] + p2 * x[n, 2, 3]) >= 0.3)
+
+    for j in 2:n
+        for i in 1:2
+            @NLconstraint(sir, x[j, i, 2] == x[j-1, i, 2] + Δt * ((-x[j-1, i, 2] * gamma) + (beta * ratio[i] * x[j-1, i, 1] * (w[i, 1] * x[j-1, 1, 2] + w[i, 2] * x[j-1, 2, 2]))))
+
+            @NLconstraint(sir, x[j, i, 1] == x[j-1, i, 1] + Δt * (-beta * ratio[i] * x[j-1, i, 1] * (w[i, 1] * x[j-1, 1, 2] + w[i, 2] * x[j-1, 2, 2])))
+
+            @NLconstraint(sir, x[j, i, 3] == x[j-1, i, 3] + Δt * (x[j-1, i, 2] * gamma))
+        end
+    end
+
+    ## Set the objective to maximimze inequity as measured by the difference in total infections between two groups at the end of the run
+    @NLobjective(sir, Max, x[n, 2, 3] / x[n, 1, 3])
+    println("Solving...")
+    return(sir)
 end
 
-## Set the objective to maximimze inequity as measured by the difference in total infections between two groups at the end of the run
-@NLobjective(sir, Max, x[n, 2, 3]/x[n, 1, 3])
-
-println("Solving...")
+sir = buildmodel()
 optimize!(sir)
 solution_summary(sir)
+
+
 
 function my_plot(y, ylabel)
     return Plots.plot(
